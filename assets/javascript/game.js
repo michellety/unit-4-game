8ffc0d28511b7361f1d8c5bdd275b40137a804f0
michelle <michelletyson29@gmail.com>
2019-01-10 23:17:36 -0800

    generate random numbers

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,22 @@
 //start the game when the page loads
+$(document).ready(function() {
+
 //computer generates a random number between 19 and 120
+var randomNum = Math.floor(Math.random() * 102) + 19;
+console.log(randomNum);
+
 //each of the 4 crystal also generate a random number, between 1 and 12
+var crystalOne = Math.floor(Math.random() * 12) + 1,
+    crystalTwo = Math.floor(Math.random() * 12) + 1,
+    crystalThree = Math.floor(Math.random() * 12) + 1,
+    crystalFour = Math.floor(Math.random() * 12) + 1;
+console.log(crystalOne, crystalTwo, crystalThree, crystalFour);
+
+
+
+});
+
+
 //score counter, starting at 0, increases the value of the crystal each time a crystal is clicked 
 //each click updates the score on the display
 //player wins if the score count matches the random number
