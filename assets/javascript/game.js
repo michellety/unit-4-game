5c3e1bb5528232ded6bc2e7cd653a52847467fcf
michelle <michelletyson29@gmail.com>
2019-01-10 22:29:34 -0800

    begin pseudocode

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+//start the game when the page loads
+//computer generates a random number between 19 and 120
+//each of the 4 crystal also generate a random number, between 1 and 12
+//score counter, starting at 0, increases the value of the crystal each time a crystal is clicked 
+//player wins if the score count matches the random number
+//the win display is increased by 1 point
+//player loses if the score count is greater than the random number 
+//the loss display is increased by 1 point 
+//the game resets the value of the random number and all crystals when win or loss is updated
