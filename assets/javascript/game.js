0ce1e95cfa8a2c8de0eac2deac5662abd9bad6bf
michelle <michelletyson29@gmail.com>
2019-01-10 22:35:34 -0800

    links

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,6 +2,7 @@
 //computer generates a random number between 19 and 120
 //each of the 4 crystal also generate a random number, between 1 and 12
 //score counter, starting at 0, increases the value of the crystal each time a crystal is clicked 
+//each click updates the score on the display
 //player wins if the score count matches the random number
 //the win display is increased by 1 point
 //player loses if the score count is greater than the random number 
